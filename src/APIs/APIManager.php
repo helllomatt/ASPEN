ee118bd9cc99c13702d3940501d342d1a64684c1
matt <me@helllomatt.com>
2017-02-09 11:52:41 -0600

    specified locations of apis now required

--- src/APIs/APIManager.php
+++ src/APIs/APIManager.php
@@ -5,72 +5,45 @@ namespace ASPEN;
 use ASPEN\Response;
 use ASPEN\Database\DB;
 
-class AppManager {
+class APIManager {
     private $folders = [];
-    private $apps = [];
+    private $apis = [];
     private $database = null;
 
-    public function __construct() {
-        $this->findApps();
-    }
-
-    private function findApps() {
-        $root = dirname(dirname(dirname(__FILE__)));
-        $possible = array_merge(scandir('apis/'), scandir('vendor/'));
-
-        foreach ($possible as $folder) {
-            if ($folder == '..' || $folder == '.') continue;
-            $app_path = 'apis/%s/controller.php';
-            $ven_path = 'vendor/%s/controller.php';
-
-            if (file_exists(sprintf($ven_path, $folder)) || file_exists(sprintf($app_path, $folder))) {
-                $this->folders[] = $folder;
-            }
-        }
-    }
+    public function __construct() { }
 
     private function error($msg = '') {
         $response = new Response();
         $response->error($msg);
     }
 
-    public function getAppFolders() {
-        return $this->folders;
-    }
-
-    public function getApps() {
-        return $this->apps;
-    }
-
     public function setDatabase(DB $db = null) {
         $this->database = $db;
     }
 
-    public function loadApps() {
+    public function load(array $locations = []) {
         $fail = [];
 
-        foreach ($this->getAppFolders() as $folder) {
-            $app_path = 'apis/'.$folder.'/controller.php';
-            $ven_path = 'vendor/'.$folder.'/controller.php';
+        foreach ($locations as $folder) {
+            $path = $folder.'/controller.php';
 
-            if (file_exists($app_path)) $app = include $app_path;
-            elseif (file_exists($ven_path)) $app = include $ven_path;
+            if (file_exists($path)) $api = include $path;
             else {
-                $this->error('App controller not found for \''.$folder.'\'');
+                $this->error('api controller not found for \''.$folder.'\'');
                 return;
             }
 
-            if ($app === 1) {
-                $this->error('Invalid app setup for \''.$folder.'\' (missing return).');
+            if ($api === 1) {
+                $this->error('Invalid api setup for \''.$folder.'\' (missing return).');
                 return;
             } else {
-                $app->setDatabase($this->database);
-                $run = $app->run();
+                $api->setDatabase($this->database);
+                $run = $api->run();
                 if (empty($run)) $fail[] = 1;
                 else $fail[] = 0;
             }
 
-            $this->apps[] = $app;
+            $this->apis[] = $api;
         }
 
         if (array_count_values($fail)[1] >= count($fail)) $this->noResponse();
