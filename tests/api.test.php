df9855d41ee826edb58fa1b12d95d9ee396dec23
matt <me@helllomatt.com>
2017-09-13 17:24:22 -0500

    uncommented test

--- tests/api.test.php
+++ tests/api.test.php
@@ -38,24 +38,24 @@ class APITest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals([true], $api->run($connector));
     }
 
-    // public function testRunningEndpointWithPrerun() {
-    //     $router = $this->getMock('ASPEN\Router', ['matches', 'getVariables']);
-    //     $router->expects($this->once())->method('matches')->will($this->returnValue(true));
-    //
-    //     $connector = $this->getMock('ASPEN\Connector', ['setData']);
-    //
-    //     $endpoint = $this->getMock('ASPEN\Endpoint', ['runCallback'], [['to' => 'test', 'preruns' => ['test']]]);
-    //     $endpoint->expects($this->once())->method('runCallback')->will($this->returnValue(function() { return true; }));
-    //
-    //     $api = (new API('name', $router))->version(1);
-    //     $api->add($endpoint);
-    //
-    //     $prerun = function() { echo "foo"; };
-    //     $api->addPreRun("test", $prerun);
-    //
-    //     $this->expectOutputString('foo');
-    //     $api->run($connector);
-    // }
+    public function testRunningEndpointWithPrerun() {
+        $router = $this->getMock('ASPEN\Router', ['matches', 'getVariables']);
+        $router->expects($this->once())->method('matches')->will($this->returnValue(true));
+
+        $connector = $this->getMock('ASPEN\Connector', ['setData']);
+
+        $endpoint = $this->getMock('ASPEN\Endpoint', ['runCallback'], [['to' => 'test', 'preruns' => ['test']]]);
+        $endpoint->expects($this->once())->method('runCallback')->will($this->returnValue(function() { return true; }));
+
+        $api = (new API('name', $router))->version(1);
+        $api->add($endpoint);
+
+        $prerun = function() { echo "foo"; };
+        $api->addPreRun("test", $prerun);
+
+        $this->expectOutputString('foo');
+        $api->run($connector);
+    }
 
     public function testRunningNonExistantPreRun() {
         $router = $this->getMock('ASPEN\Router', ['matches', 'getVariables']);
